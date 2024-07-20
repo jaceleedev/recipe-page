620119419decea7b4cf1f0ea4ae9d16c8f3f6a10
jaceleedev <jaceleedev@gmail.com>
2024-07-21 01:45:32 +0900

    fix: :lipstick: set fixed width for consistent layout

--- src/components/RecipePage/index.tsx
+++ src/components/RecipePage/index.tsx
@@ -1,7 +1,15 @@
 import styles from './index.module.css';
+import RecipeImage from './RecipeImage';
 
 function RecipePage() {
-  return <div className={styles['recipe-page']}>Recipe page</div>;
+  return (
+    <div className={styles['recipe-page']}>
+      <RecipeImage
+        src={'/assets/images/image-omelette.jpeg'}
+        alt={'simple omelette'}
+      />
+    </div>
+  );
 }
 
 export default RecipePage;
