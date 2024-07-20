1a76a9578d06e7d8cb2597ad6152b21c8bfde8fc
jaceleedev <jaceleedev@gmail.com>
2024-07-21 01:44:57 +0900

    feat: :sparkles: create RecipeImage component

---
+++ src/components/RecipePage/RecipeImage/index.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import styles from './index.module.css';
+
+interface RecipeImageProps {
+  src: string;
+  alt: string;
+}
+
+function RecipeImage({ src, alt }: RecipeImageProps) {
+  return (
+    <div className={styles['recipe-image']}>
+      <Image src={src} alt={alt} fill priority />
+    </div>
+  );
+}
+
+export default RecipeImage;
