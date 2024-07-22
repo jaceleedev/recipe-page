5764faecb27d1871d00c9ca57d4a805b48e3b444
jaceleedev <jaceleedev@gmail.com>
2024-07-22 21:35:19 +0900

    refactor: :recycle: enhance RecipeImage component for accessibility and semantics

--- src/components/RecipePage/RecipeImage/index.tsx
+++ src/components/RecipePage/RecipeImage/index.tsx
@@ -1,16 +1,22 @@
 import Image from 'next/image';
-import styles from './index.module.css';
 
 interface RecipeImageProps {
   src: string;
   alt: string;
 }
 
-function RecipeImage({ src, alt }: RecipeImageProps) {
+function RecipeImage({ src, alt }: Readonly<RecipeImageProps>) {
   return (
-    <div className={styles['recipe-image']}>
-      <Image src={src} alt={alt} fill priority className={styles.img} />
-    </div>
+    <figure className="relative w-full h-[300px] rounded-xl overflow-hidden mobile:h-[171px]">
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        priority
+        className="tablet:object-cover"
+      />
+      <figcaption className="sr-only">{alt}</figcaption>
+    </figure>
   );
 }
 
