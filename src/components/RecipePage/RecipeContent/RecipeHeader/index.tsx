2d588243b00a5b3e5a903107c2f5eda9f5fe82d8
jaceleedev <jaceleedev@gmail.com>
2024-07-22 21:41:16 +0900

    refactor: :recycle: optimize RecipeHeader for accessibility and performance

--- src/components/RecipePage/RecipeContent/RecipeHeader/index.tsx
+++ src/components/RecipePage/RecipeContent/RecipeHeader/index.tsx
@@ -1,17 +1,15 @@
-import styles from './index.module.css';
-
 function RecipeHeader() {
   return (
-    <div className={styles['recipe-header']}>
-      <h1 className={styles['recipe-header__heading']}>
+    <header className="flex flex-col items-start gap-300">
+      <h1 className="self-stretch text-preset-1 text-stone-900">
         Simple Omelette Recipe
       </h1>
-      <p className={styles['recipe-header__paragraph']}>
+      <p className="text-preset-4 text-stone-600">
         An easy and quick dish, perfect for any meal. This classic omelette
         combines beaten eggs cooked to perfection, optionally filled with your
         choice of cheese, vegetables, or meats.
       </p>
-    </div>
+    </header>
   );
 }
 
